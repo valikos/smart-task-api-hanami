fff6ee1f4e340bb969830b058b8bfa6b39dcc4a3
Valentyn Ostakh <valikos.ost@gmail.com>
2016-12-09 11:25:00 +0200

    Fix relations for jsonapi

--- apps/api/serializers/tasks/task_serializer.rb
+++ apps/api/serializers/tasks/task_serializer.rb
@@ -1,4 +1,8 @@
 class TaskSerializer < BaseSerializer
   attribute :title
   attribute :completed
+
+  has_one :project, include_data: true do
+    ProjectRepository.new.find(self.object.project_id)
+  end
 end
