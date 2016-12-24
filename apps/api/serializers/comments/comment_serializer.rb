510c00e2834aeb8aae206451f2d71feb5847de4d
Valentyn Ostakh <valikos.ost@gmail.com>
2016-12-24 14:10:53 +0200

    Serialize relationships with data

--- apps/api/serializers/comments/comment_serializer.rb
+++ apps/api/serializers/comments/comment_serializer.rb
@@ -2,7 +2,7 @@ class CommentSerializer < BaseSerializer
   attribute :body
   attribute :created_at
 
-  has_one :task do
+  has_one :task, include_data: true do
     TaskRepository.new.find(self.object.task_id)
   end
 end
--- apps/api/serializers/projects/project_serializer.rb
+++ apps/api/serializers/projects/project_serializer.rb
@@ -1,7 +1,7 @@
 class ProjectSerializer < BaseSerializer
   attribute :title
 
-  has_many :tasks do
+  has_many :tasks, include_data: true do
     ProjectRepository.new.get_tasks(self.object)
   end
 end
